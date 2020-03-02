01fb340a99aec8c627fcb564bbf7ac87dcf1557f
zhangshan <sangazh@outlook.com>
2020-03-02 18:27:49 +0800

    add diamond

--- go/diamond/diamond.go
+++ go/diamond/diamond.go
@@ -4,26 +4,62 @@ import (
 	"errors"
 )
 
-func Gen(b byte) (string, error) {
-	order := int(b - 'A' + 1)
-
-	if order < 0 || order > 26 {
-		return "", errors.New("out or range")
+func Gen(b byte) (s string, err error) {
+	if b < 'A' || b > 'Z' {
+		return "", errors.New("out of range")
 	}
-	if order == 1 {
-		return "A", nil
+	n := b - 'A' + 1
+
+	if n == 1 {
+		return "A\n", nil
 	}
 
-	height := order*2 - 1
-	result := make([][]byte, height)
-	for i := 0; i < height; i++ {
-		for j := 0; j < height; j++ {
-			if j == order {
+	for i := uint8(1); i <= n; i++ {
+		s += render(i, n)
+	}
 
-			}
-			result[i][j] = '.'
+	for i := n - 1; i > 0; i-- {
+		s += render(i, n)
+	}
+	return s, nil
+}
+
+func render(i, n uint8) (s string) {
+	if i == 1 {
+		return renderRowContainsOneA(n)
+	}
+	return renderOtherRows(i, n)
+}
+
+func renderRowContainsOneA(n uint8) (s string) {
+	for i := uint8(0); i < 2*n-1; i++ {
+		if i == n-1 {
+			s += "A"
+		} else {
+			s += " "
+
+		}
+	}
+	return s + "\n"
+}
+
+func renderOtherRows(i, n uint8) (s string) {
+	char := string('A' + i - 1)
+	for j := n; j > 0; j-- {
+		if j == i {
+			s += char
+		} else {
+			s += " "
 		}
 	}
 
-	return "", nil
+	for j := uint8(2); j <= n; j++ {
+		if j == i {
+			s += char
+		} else {
+			s += " "
+		}
+	}
+
+	return s + "\n"
 }
